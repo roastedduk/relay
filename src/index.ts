bb8b89cc1df5033ff5caccf76079ed62624bc007
RoastedDuck <roasted@ducc.pw>
2025-07-14 14:44:28 +0700

    feat: add video note support

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,7 @@ bot.command("relay", async (ctx) => {
 		const text = messageToForward.text;
 		const caption = messageToForward.caption;
 		const sticker = messageToForward.sticker;
+		const videoNote = messageToForward.video_note;
 
 		const promises = [];
 
@@ -58,6 +59,8 @@ bot.command("relay", async (ctx) => {
 				promises.push(ctx.api.sendSticker(sender.id, sticker.file_id))
 			} else if (text) {
 				promises.push(ctx.api.sendMessage(sender.id, text))
+			} else if (videoNote) {
+				promises.push(ctx.api.sendVideoNote(sender.id, videoNote.file_id));
 			} else {
 				await ctx.reply("Unsupported message type.", {
 					reply_parameters: {
